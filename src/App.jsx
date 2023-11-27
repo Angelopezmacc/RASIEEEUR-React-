eaecdc5670c2c16e1455bf81a1691aa519410dea
jecruzreichert <jecruzreichert@gmail.com>
2023-11-27 01:15:45 -0500

    Primera parte del enrutamiento

--- src/App.jsx
+++ src/App.jsx
@@ -3,41 +3,19 @@ import input from '../public/img/input.svg'
 import log_in_button from '../public/img/bx-right-arrow-alt.svg'
 import logo from '../public/img/RASIEEE.png'
 import Registro from './Registro.jsx'
+import LogIn from './LogIn.jsx'
+import { Route } from 'react-router'
+import { Routes } from 'react-router'
+
+const Home = () => <h1>Home</h1>
+
 function App() {
   return (
     <>
-    <div className="contenedor_flex">
-        <div className="columna1">
-          <h2>Bienvenido al equipo mas pro de Colombia</h2>
-          <img src={logo} alt="" className='logo_grande'/>
-        </div>
-        <div className="columna2">
-          <div className='f_log_in'>
-            <h1>Log in</h1>
-            <form action="">
-              <div className="campo">
-                  <input type="text" name="" id="" required / >
-                  <label htmlFor="" className='label'><span className='texto_input'>Usuario</span></label>
-                  <img src={input} className='input_svg' alt="" />
-              </div>
-
-              <div className="campo">
-                  <input type="password" name="" id="" required/>
-                  <label htmlFor="" className='label'><span className='texto_input'>Contraseña</span></label>
-                  <img src={input} className='input_svg' alt="" />
-              </div>
-            </form>
-            <div className='botones_log'>
-              <a href="" className='olvide_contraseña'>Olvide mi contraseña</a>
-              <div className='log_in_boton'>
-                <img src={log_in_button} alt="" />
-                <p>Entrar</p>
-              </div>
-            </div>
-        </div>
-        </div>
-    </div>
-    <Registro />
+    <Routes>
+      <Route path='/' element={<LogIn/>}></Route>
+      <Route path='/registro' element={<Registro/>}></Route>
+    </Routes>
     </>
   )
 }
