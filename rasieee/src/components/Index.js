3cb9c2ad69ebea2059ebd6e2e34f387a9f502549
NicolasEGallego <92643056+NicolasEGallego@users.noreply.github.com>
2023-11-29 10:23:47 -0500

    .yonocommit

--- rasieee/src/components/Index.js
+++ rasieee/src/components/Index.js
@@ -3,6 +3,12 @@ import { Carousel } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../css/Index.css";
 import companyImage from "../img/RASIEEE3.png";
+import competencia1 from  "../img/RoPF.webp";
+import competencia2 from "../img/Runbt.jpg";
+import competencia3 from "../img/utprobo.jpg";
+import rbt1 from "../img/futro.webp";
+import rbt2 from "../img/seguidor.jpg";
+import rbt3 from "../img/sumo.webp";
 import { useRef } from "react";
 
 const Index = () => {
@@ -37,23 +43,82 @@ const handleClick3 = () => {
           </div>
         </div>
         <div className="hScroll_block caja2">
-          <div className="background" ref={ref1}></div>
+          <div className="background" ref={ref1}>
+            <div className="carusel1">
+            <Carousel id="carouselExample">
+            <Carousel.Item>
+              <img
+                className="d-block w-100 carousel-image"
+                src={competencia1}
+                alt="First slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100 carousel-image"
+                src={competencia2}
+                alt="Second slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+            <img
+                className="d-block w-100 carousel-image"
+                src={competencia3}
+                alt="Second slide"
+              />
+            </Carousel.Item>
+              {/* Agrega más elementos Carousel.Item según sea necesario con tus imágenes importadas */}
+            </Carousel>
+            </div>
+            <div className="carusel2">
+            <Carousel id="carouselExample">
+            <Carousel.Item>
+              <img
+                className="d-block w-100 carousel-image"
+                src={rbt1}
+                alt="First slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100 carousel-image"
+                src={rbt2}
+                alt="Second slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+            <img
+                className="d-block w-100 carousel-image"
+                src={rbt3}
+                alt="Second slide"
+              />
+            </Carousel.Item>
+              {/* Agrega más elementos Carousel.Item según sea necesario con tus imágenes importadas */}
+            </Carousel>
+            </div>
+          </div>
         </div>
         <div className="hScroll_block">
           <div className="background" ref={ref2}></div>
         </div>
         <div className="hScroll_block">
           <div className="background" ref={ref3}>
-          <iframe
+            <div className="invita">
+            <iframe
               width="500px"
               height="600px"
               src="https://forms.office.com/Pages/ResponsePage.aspx?id=V1dSrrqJME2i90l5bvjGBFT-fMaN4a5Ol9E3IFlJ4K5UNlVSUVJXUkdYUlQ3T1VRS0lOOExLWDJOQS4u&embed=true"
-              style={{ border: "none", maxWidth: "auto", maxHeight: "80%", marginTop: "5%", marginRight: "50%" }}
+              style={{ border: "none", maxWidth: "auto", maxHeight: "80%", marginTop: "5%", marginLeft: "0%" }}
               allowFullScreen
               webkitAllowFullScreen
               mozAllowFullScreen
               msAllowFullScreen
             ></iframe>
+            </div>
+            <div className="invita">
+              <h2 className="preg">¿Quieres unirte a nuestro equipo?</h2>
+              <t className="tx1">Llena el formulario de inscripción, te enviaremos un mensaje a tu correo institucional para continuar el proceso y que hagas partes del equipo.</t>
+            </div>
           </div>
         </div>
         <div className="hScroll_block" ref={ref4}>
@@ -66,16 +131,16 @@ const handleClick3 = () => {
       <nav className="navbarra">
         <img src={companyImage} alt="Logo" width="auto" height="80%" />
         <div className="inpage">
-          <button ClassName="bt1" onClick={handleClick}>
+          <button className="bt1" onClick={handleClick}>
             Inicio
           </button>
-          <button ClassName="bt1" onClick={handleClick1}>
+          <button className="bt1" onClick={handleClick1}>
             Nuestro Equipo
           </button>
-          <button ClassName="bt1" onClick={handleClick2}>
+          <button className="bt1" onClick={handleClick2}>
             Quieres Unirte
           </button>
-          <button ClassName="bt1" onClick={handleClick3}>
+          <button className="bt1" onClick={handleClick3}>
             Proyectos
           </button>
         </div>
